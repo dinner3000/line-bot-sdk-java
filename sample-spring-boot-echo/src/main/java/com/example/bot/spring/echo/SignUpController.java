712ef08607401b3a8d404d28c9e1f8ff6e76b672
Gary.Z <dinner3000@hotmail.com>
2020-03-28 23:48:54 +0800

    feat: add a test API

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/SignUpController.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/SignUpController.java
@@ -3,10 +3,7 @@ package com.example.bot.spring.echo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/")
@@ -19,9 +16,15 @@ public class SignUpController {
         this.newFollowerTemporaryStorage = newFollowerTemporaryStorage;
     }
 
+    @GetMapping
+    @ResponseBody
+    public Object ok(@ModelAttribute SignUpDTO signUpDTO) {
+        return "OK";
+    }
+
     @PostMapping("signup")
     @ResponseBody
-    public Object greetingSubmit(@ModelAttribute SignUpDTO signUpDTO) {
+    public Object signUp(@ModelAttribute SignUpDTO signUpDTO) {
 
         NewFollowerEntity newFollowerEntity = newFollowerTemporaryStorage.get(signUpDTO.getNonce());
         signUpDTO.setNewFollower(newFollowerEntity);
